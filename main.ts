b01e8e46db7c4816812aa612bf136dc613cbce77
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-03-13 08:29:07 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,25 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         carcotrol.setNeoColor(carcotrol.colors(RGBColors.Green))
+        carcotrol.setLED(Position.Both, carcotrol.colors(RGBColors.Green))
     } else if (receivedNumber == 2) {
         carcotrol.setNeoColor(carcotrol.colors(RGBColors.Red))
+        carcotrol.setLED(Position.Both, carcotrol.colors(RGBColors.Red))
     } else if (receivedNumber == 3) {
         carcotrol.setNeoColor(carcotrol.colors(RGBColors.Blue))
+        carcotrol.setLED(Position.Both, carcotrol.colors(RGBColors.Blue))
     } else if (receivedNumber == 4) {
         carcotrol.setNeoColor(carcotrol.colors(RGBColors.Yellow))
+        carcotrol.setLED(Position.Both, carcotrol.colors(RGBColors.Yellow))
     } else if (receivedNumber == 5) {
         carcotrol.setNeoColor(carcotrol.colors(RGBColors.White))
+        carcotrol.setLED(Position.Both, carcotrol.colors(RGBColors.White))
     } else if (receivedNumber == 6) {
         carcotrol.setNeoColor(carcotrol.colors(RGBColors.Purple))
+        carcotrol.setLED(Position.Both, carcotrol.colors(RGBColors.Purple))
     } else {
         carcotrol.setNeoColor(carcotrol.colors(RGBColors.Black))
+        carcotrol.setLED(Position.Both, carcotrol.colors(RGBColors.Black))
     }
 })
 radio.onReceivedString(function (receivedString) {
